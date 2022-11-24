12a6e59c17066ca63cfa71a2e1779c4f4a31437a
Deepa Bharti <76946978+Deepa-47@users.noreply.github.com>
2022-11-24 13:59:31 +0530

    Add files via upload

--- innodeed-mycars-coding-test/src/component/Carosel.js
+++ innodeed-mycars-coding-test/src/component/Carosel.js
@@ -1,37 +1,44 @@
-import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+
 import { cardata } from "../database/api";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import { CarModelCard } from "./CarModelCard";
 import './styles.css'
-import back from "./assets/back.png";
-import next from "./assets/next.png";
+import "swiper/css/navigation";
 
 // import required modules
-import { Pagination, Autoplay } from "swiper";
+import { Pagination, Autoplay, Navigation  } from "swiper";
 
 export default function Carosel() {
   const data=cardata;
-  const [screenWidth, setScreewidth] = useState(window.innerWidth);
+ 
   return (
     <>
       <Swiper
-
-        slidesPerView={screenWidth>600?3:1}
-        spaceBetween={30}
-        navigation={true} 
-        pagination={{
-          clickable: true,
+        navigation={true}
+        pagination={true}
+        breakpoints={{
+          800: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
         }}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
-        modules={[Pagination, Navigation, Autoplay]}
+        modules={[Navigation, Pagination, Autoplay]}
         className="mySwiper"
       >
         {data.map((data)=>{
@@ -41,22 +48,9 @@ export default function Carosel() {
                 </div>
             )
         })} 
-        {/* <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
+        
       </Swiper>
-      <div className="caroselButton">
-        <div className="svgIcon">
-        <img src={next} />
-        <img src={back} />
-        </div>
-      </div>
+      
     </>
   );
 }
